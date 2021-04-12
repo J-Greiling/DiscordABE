ab55f8c574e8fc2951f8cdf40f6ab6267514ed60
Jake <jakob.greiling@gmail.com>
2021-04-12 18:26:43 +0200

    reworked resource managment to pandas

--- resource/resmanagement.py
+++ resource/resmanagement.py
@@ -4,32 +4,38 @@ __author__ = "Jakob Greiling"
 __date__ = "2021-03"
 
 import discord
+import pandas as pd
 from discord.ext import commands
-from . import resource
 
 
 class ResManagement(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.resource = resource.Resource()
+        filename = "./text/resource.csv"
+        self.resource = pd.read_csv(
+            filename, delimiter=";", header=0, encoding="utf-8")
+        self.resource["emoji"] = [emote.encode().
+                                  decode("unicode-escape")
+                                  for emote in self.resource["emoji"]]
+
         self.res_msg = 0
 
     @commands.command(name="storage", help="Information on Money and Ressource Managment of the Guild")
     async def print_storage(self, ctx):
         embed = discord.Embed(title="Storage")
-        for i in range(len(self.resource.name)):
-            if self.resource.max[i] == "0":
+        for i in range(len(self.resource)):
+            if self.resource['max'][i] == 0:
                 embed.add_field(
-                    name=f"{self.resource.name[i]}-{self.resource.emote[i]}",
-                    value=f"{self.resource.current[i]}")
+                    name=f"{self.resource['resource_name'][i]}-{self.resource['emoji'][i]}",
+                    value=f"{self.resource['current'][i]}")
             else:
                 embed.add_field(
-                    name=f"{self.resource.name[i]}-{self.resource.emote[i]}",
-                    value=f"{self.resource.current[i]} / {self.resource.max[i]}")
+                    name=f"{self.resource['resource_name'][i]}-{self.resource['emoji'][i]}",
+                    value=f"{self.resource['current'][i]} / {self.resource['max'][i]}")
 
         msg = await ctx.send(embed=embed)
         self.res_msg = msg
-        for emote in self.resource.emote:
+        for emote in self.resource['emoji']:
             await msg.add_reaction(emote)
 
     @commands.command(name="add_money", help="Adds Money to the Guild")
